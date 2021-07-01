6f19a36c9f6a5dcc7c6528c2c4cb8791c90885ea
osvaldosneto <osvaldosneto@hotmail.com>
2021-07-01 00:31:03 -0300

    Funcoes de entrada prontas

--- pointgame/testes/server/server.js
+++ pointgame/testes/server/server.js
@@ -3,11 +3,12 @@ const WebSocket = require('ws');
 const SocketServer = WebSocket.Server;
 const server = express().listen(8082);
 const wss = new SocketServer({server});
+const randomToken = require('random-token');
 
-const users_id = []
-const users_ws = []
-const cod = '200'
-
+const maxElementosSala = 5
+let user = {}
+let users = []
+const salas = []
 
 wss.on('connection', (ws) => {
     console.log('[Servidor] Um cliente se conectou!')
@@ -15,19 +16,32 @@ wss.on('connection', (ws) => {
     ws.on('close', () => { console.log('[Servidor] Um cliente se desconectou!') })
 
     ws.on('message', (msg) => { 
-        
         var req = msg.split(":")
         console.log(req)
-
         switch(req[0]){
             case 'entrar':
-                if (entrada(req[1],req[2])) {
-                    id = random_id()
-                    users_ws.push(ws)
-                    users_id.push(id)
-                    ws.send("ok:"+id+":"+cod)
+                if(verifica_sala(req[2])){
+                    let verificaNome = verifica_nome(req[1])
+                    if(verificaNome===200){
+                        //verificacao ok
+                        user.id = req[1]
+                        user.token = randomToken(16)
+                        user.ws = ws
+                        user.sala = req[2]
+                        user.ws.send("ok:"+user.token+":200")
+                        users.push(user)
+                        adiciona_usuario_sala(user)
+                    } else if (verificaNome===401){
+                        //problemas na verificacao usuario incorreto
+                        ws.send("nok:401")
+                        ws.close()
+                    } else {
+                        //usuario duplicado
+                        ws.send("nok:400")
+                        ws.close()
+                    }
                 } else {
-                    ws.send("nok:"+cod)
+                    ws.send("nok:402")
                     ws.close()
                 } 
                 break;
@@ -43,41 +57,52 @@ wss.on('connection', (ws) => {
 
 })
 
-// Tratamentos de entrada do cliente
-entrada = (nome,sala) => {
-    if (verifica_nome(nome)){
-        if (verifica_sala(sala)) {
-            cod = '200'
-            return true
-        } else {
-            cod = '402'
-            return false
-        }
-    } else {
-        // cod = '400' NOME DUPLICADO
-        cod = '400' // NOME ERRADO
-        return false    
-    }
-}
-
 // Verifica se o nome do cliente é válido
 verifica_nome = (nome) => {
-    return true
+    if(nome.includes(" ") || (nome.includes(":"))){
+        return 401
+    }
+    userFilter = users.filter((user) => {
+        return user.id === nome
+    })
+    if(userFilter.length > 0){
+        return 400
+    }
+    return 200
 }
 
 // Verifica se a sala existe
-verifica_sala = (sala) => {
+verifica_sala = (salaObj) => {
     // nome da sala errada - (onde vai estar esses nomes???)
     // sala cheia - (onde vai estar essa informação???)
+    const salaFilter = salas.filter( (sala) => {
+        return sala.nome === salaObj
+    })
+    if(salaFilter.length > 0){
+        const salaPop = salaFilter.pop()
+        if(salaPop.qtdade >= maxElementosSala){
+            return false
+        }
+    }
     return true
 }
 
-// Cria ID aleatório para o usuário
-random_id = () => {
-    var a = ['a','b','c','d','e','f','g','h','i','j']
-    var A = ['A','B','C','D','E','F','G','H','I','J']
-    ts = new Date().getTime().toString()
-    return a[ts[12]] + a[ts[11]] + A[ts[10]] + A[ts[9]]
+
+adiciona_usuario_sala = (usuario) => {
+    let salaFilter = salas.filter( (sala) => {
+        return sala.nome === usuario.sala
+    })
+    if(salaFilter.length > 0){
+        salas.forEach( sala => {
+            if(sala.nome === usuario.sala){
+                sala.qtdade ++
+            }
+        })
+    } else {
+        obj = {
+            nome: usuario.sala,
+            qtdade: 1
+        }
+        salas.push(obj)
+    }
 }
-
-
