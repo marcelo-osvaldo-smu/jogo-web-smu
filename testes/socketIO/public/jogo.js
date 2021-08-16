3469484f236490e4ca5de52171b402d1735de42c
marcelo.bn <marcelobittencourtx@gmail.com>
2021-08-16 09:36:16 -0300

    marcelo 2

--- testes/socketIO/public/jogo.js
+++ testes/socketIO/public/jogo.js
@@ -1,4 +1,5 @@
 var socket = io('http://localhost:3000');
+var socket = io("/",{path: "marcelobn/socket.io"});
 var idSocket = ""
 // branch 2
 var usuario = {}
@@ -8,8 +9,19 @@ var salas = {}
 var fx = fy = 0
 var primeiro = false
 var lista_usuarios = {}
-var ice_servers = {
+/*var ice_servers = {
     iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+};*/
+
+var ice_servers = {
+    iceServers: [
+      { urls: "stun:smu20211.sj.ifsc.edu.br" },
+      {
+        urls: "turn:smu20211.sj.ifsc.edu.br",
+        username: "marcelo.bn",
+        credential: "smu20211"
+      }
+    ],
 };
 var localConnection;
 var remoteConnection;
